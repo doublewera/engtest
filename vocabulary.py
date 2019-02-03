24894e4fd64a8ef797b73a829c04e4b39380dc5e
Sverdlovchanin <bela-182@mail.ru>
2019-02-03 16:59:55 +0300

    Added save_dict

--- vocabulary.py
+++ vocabulary.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-
 import os
 
 
@@ -10,7 +9,24 @@ if os.path.exists("mydict.py"):
 
 
 def fill_dict(en_ru):
+    new_word = None
     # side-effect: en_ru is being changed here
+    while new_word != "":
+        new_word = input("Введите слово ")
+        new_translate = input("Введите перевод ")
+
+        if new_word not in en_ru and new_word != "":
+            en_ru[new_word] = new_translate
     return
 
+
+
+def save_dict(en_ru, dict_file_name="mydict.py"):
+    mydict = open(dict_file_name, "w")
+    mydict.write("en_ru = %s" % en_ru)
+    mydict.close()
+    return
+
+fill_dict(en_ru)
+save_dict(en_ru)
 print(en_ru)

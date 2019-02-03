105745266fe87815d765a551a8d673b3642aa979
Wera Barinova <alisawera@gmail.com>
2019-02-03 16:35:37 +0300

    Fill dictionary from the raw input

--- vocabulary.py
+++ vocabulary.py
@@ -10,7 +10,15 @@ if os.path.exists("mydict.py"):
 
 
 def fill_dict(en_ru):
+    new_word = None
     # side-effect: en_ru is being changed here
+    while new_word != "":
+        new_word = input("Введите слово ")
+        new_translate = input("Введите перевод ")
+
+        if new_word not in en_ru and new_word != "":
+            en_ru[new_word] = new_translate
     return
 
+fill_dict(en_ru)
 print(en_ru)
